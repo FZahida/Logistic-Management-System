3ee039ba4d1998fe9c7537fe3d33fdcf6f00f1ab
FZahida <fathimazahida24@gmail.com>
2025-10-25 22:29:42 +0530

    'inputDistance' method and 'editDistance' method added.

--- ICT1011_assignment_AS20240918/src/ict1011_assignment_as20240918/ICT1011_assignment_AS20240918.java
+++ ICT1011_assignment_AS20240918/src/ict1011_assignment_as20240918/ICT1011_assignment_AS20240918.java
@@ -9,7 +9,10 @@ public class ICT1011_assignment_AS20240918 {
      
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        
         String[] cities = new String[MAX_CITIES];
+        int[][] distance = new int[MAX_CITIES][MAX_CITIES];
+
         do {
             printMenu();
             choice=sc.nextInt();
@@ -19,7 +22,7 @@ public class ICT1011_assignment_AS20240918 {
                     cityManagement(cities);
                     break;   
                 case 2:
-                    distanceManagement();
+                    distanceManagement(distance);
                     break;
                 case 3:
                     vehicleManagement();
@@ -121,38 +124,95 @@ public class ICT1011_assignment_AS20240918 {
             System.out.println(i+1 +"."+cities[i]);      
         }    
     }
-    public static void distanceManagement(){
+    public static void distanceManagement(int[][]distance,String[]cities){
         Scanner sc= new Scanner(System.in);
          do {
              System.out.println("=====Distance Management Menu ===== ");
              System.out.println("1. Input distance between two cities");
              System.out.println("2. Edit distance between two cities");
-             System.out.println("3. Back to the main menu");
+             System.out.println("3. View distance table");
+             System.out.println("4. Back to the main menu");
              System.out.println("Enter your choice: ");
              choice = sc.nextInt();
              sc.nextLine();
  
             switch (choice) {
                 case 1:
-                    inputDistance();
+                    inputDistance(distance);
                     break;
                 case 2:
-                    editDistance();
+                    editDistance(distance,cities);
                    break;
                 case 3:
+                    distanceTable();
+                    break;
+                case 4:
                     System.out.println("Return to the main menu");
                     break;
                 default:
                     System.out.println("Invalid choice.");
             }
-        } while (choice != 3);        
+        } while (choice != 4);        
     }
-    public static void inputDistance(){
+    public static void inputDistance(int[][] distance){
+        Scanner sc=new Scanner(System.in);
+        if(cityCount<2){
+            System.out.println("Add at least 2 cities first");
+            return;
+        }
+        System.out.print("Enter source city index :");
+        int source=sc.nextInt();
+        System.out.print("Enter destination city index :");
+        int destination=sc.nextInt();
+        
+        if(source==destination){
+            System.out.println("Source city and Destination city cannot be same");
+            return;
+        }
+        if(distance[source][destination]!=0){
+            System.out.println("Distance already exists.");
+            return;           
+        }
+        System.out.print("Enter distance in (km): ");
+        int dist=sc.nextInt();
+        
+        distance[source][destination]=dist;
+        distance[destination][source]=dist;
+        
+        System.out.println("Distance added successfully");   
+    }
+    public static void editDistance(int[][]distance,String[]cities){
+        Scanner sc=new Scanner(System.in);
+        if(cityCount<2){
+            System.out.println("Add at least 2 cities first");
+            return;
+        }
+        System.out.print("Enter source city index :");
+        int source=sc.nextInt();
+        System.out.print("Enter destination city index :");
+        int destination=sc.nextInt();
+        
+        if(source==destination){
+            System.out.println("Invalid!...Source city and Destination city cannot be same");
+            return;
+        }
+         if(distance[source][destination] == 0){
+            System.out.println("No existing distance found.");
+            return;           
+        }
+        System.out.println("Current distance between"+ cities[source]+"and"+cities[destination]+":"+distance[source][destination]+"km");
+        System.out.print("Enter new distance (km): ");
+        int NDist=sc.nextInt();
+        
+        distance[source][destination]=NDist;
+        distance[destination][source]=NDist;
+        
+        System.out.println("Distance updated successfully");           
+    }
+    public static void distanceTable(){
         
     }
-    public static void editDistance(){
-        
-    }
+
     
     public static void vehicleManagement(){
         
